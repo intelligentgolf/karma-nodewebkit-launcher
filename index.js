1634818130075f3cae2c94e301a0a6f894d35726
Michal Charemza <michalcharemza@gmail.com>
2015-03-15 20:04:35 +0000

    Paranoia using path.join for Windows

--- index.js
+++ index.js
@@ -16,29 +16,29 @@ var NodeWebkitBrowser = function(baseBrowserDecorator, args) {
 
   this._start = function(url) {
     var self = this;
-    var SOURCE_PATH = path.normalize(__dirname + '/runner.nw');
-    var STATIC_PATH = path.normalize(self._tempDir + '/runner.nw');
-    var INDEX_HTML = '/index.html';
-    var PACKAGE_JSON = '/package.json';
+    var SOURCE_PATH = path.join(__dirname, 'runner.nw');
+    var STATIC_PATH = path.join(self._tempDir, 'runner.nw');
+    var INDEX_HTML = path.join(STATIC_PATH, 'index.html');
+    var PACKAGE_JSON = path.join(STATIC_PATH, 'package.json');
 
     async.auto({
       'directory': function(callback) {
         ncp(SOURCE_PATH, STATIC_PATH, callback);
       },
       'index.html:read': ['directory', function(callback) {
-        fs.readFile(STATIC_PATH + INDEX_HTML, callback);
+        fs.readFile(INDEX_HTML, callback);
       }],
       'index.html:write': ['index.html:read', function(callback, results) {
         var content = results['index.html:read'].toString().replace('%URL%', url);
-        fs.writeFile(STATIC_PATH + INDEX_HTML, content, callback);
+        fs.writeFile(INDEX_HTML, content, callback);
       }],
       'package.json:read': ['directory', function(callback) {
-        fs.readFile(STATIC_PATH + PACKAGE_JSON, callback);
+        fs.readFile(PACKAGE_JSON, callback);
       }],
       'package.json:write': ['package.json:read', function(callback, results) {
         var options = JSON.parse(results['package.json:read'].toString());
         options = merge(true, options, customOptions);
-        fs.writeFile(STATIC_PATH + PACKAGE_JSON, JSON.stringify(options), callback);
+        fs.writeFile(PACKAGE_JSON, JSON.stringify(options), callback);
       }],
       'exec': ['index.html:write', 'package.json:write', function(callback) {
         process.env.NODE_PATH = searchPaths.join(path.delimiter);
